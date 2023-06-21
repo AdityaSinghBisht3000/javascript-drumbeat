2d858ee6fc12f7298c3ba0ecb02805b36ce4bd64
Aditya Singh Bisht <115521200+AdityaSinghBisht3000@users.noreply.github.com>
2023-06-21 12:36:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.querySelectorAll(".drum")[0].addEventListener("click",function ()
 {
 //  alert("hi");
 addAnimation(this.innerHTML);
-   var audio=new Audio('./sounds/tom-1.mp3');
+   var audio=new Audio('./tom-1.mp3');
    audio.play(); 
 //    this.
 });
@@ -13,7 +13,7 @@ document.querySelectorAll(".drum")[1].addEventListener("click",function ()
 
     addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/tom-2.mp3');
+    var audio=new Audio('./tom-2.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -21,7 +21,7 @@ document.querySelectorAll(".drum")[2].addEventListener("click",function ()
 {
 addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/tom-3.mp3');
+    var audio=new Audio('./tom-3.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -30,7 +30,7 @@ document.querySelectorAll(".drum")[3].addEventListener("click",function ()
 
 addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/tom-4.mp3');
+    var audio=new Audio('./tom-4.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -39,7 +39,7 @@ document.querySelectorAll(".drum")[4].addEventListener("click",function ()
 
 addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/snare.mp3');
+    var audio=new Audio('./snare.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -48,7 +48,7 @@ document.querySelectorAll(".drum")[5].addEventListener("click",function ()
 
 addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/crash.mp3');
+    var audio=new Audio('./crash.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -57,7 +57,7 @@ document.querySelectorAll(".drum")[6].addEventListener("click",function ()
 
 addAnimation(this.innerHTML);
 
-    var audio=new Audio('./sounds/kick-bass.mp3');
+    var audio=new Audio('./kick-bass.mp3');
    audio.play(); 
 //  alert("hi");
 });
@@ -73,37 +73,37 @@ addAnimation(this.innerHTML);
         switch(event.key)
         {
             case "w":
-                var audio=new Audio('./sounds/tom-1.mp3');
+                var audio=new Audio('./tom-1.mp3');
                 audio.play(); 
             break;
             case "a":
-                var audio=new Audio('./sounds/tom-2.mp3');
+                var audio=new Audio('./tom-2.mp3');
                 audio.play(); 
 
             break;
             case "s":
-                var audio=new Audio('./sounds/tom-3.mp3');
+                var audio=new Audio('./tom-3.mp3');
                 audio.play(); 
 
             break;
             case "d":
-                var audio=new Audio('./sounds/tom-4.mp3');
+                var audio=new Audio('./tom-4.mp3');
                 audio.play(); 
 
             break;
             case "j":
-                var audio=new Audio('./sounds/snare.mp3');
+                var audio=new Audio('./snare.mp3');
                 audio.play(); 
 
 
             break;
             case "k":
-                var audio=new Audio('./sounds/crash.mp3');
+                var audio=new Audio('./crash.mp3');
                 audio.play(); 
 
             break;
             case "l":
-                var audio=new Audio('./sounds/kick-bass.mp3');
+                var audio=new Audio('./kick-bass.mp3');
                 audio.play(); 
 
             break;
